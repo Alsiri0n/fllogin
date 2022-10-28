eb9fd079342d45481e4632b05a35a0159f2d36fd
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-10-28 22:19:21 +0300

    Add changes to work with sql

--- fllogin.py
+++ fllogin.py
@@ -3,19 +3,21 @@ Login submodule for flask application
 """
 from flask import Blueprint,render_template,request,flash, session, redirect, url_for, g
 from werkzeug.security import generate_password_hash
+from flsql.flsql import Flsql
 
 fllogin = Blueprint('fllogin', __name__, template_folder='templates', static_folder='static')
 
+db = None
 dbase = None
-
 
 @fllogin.before_request
 def before_request():
     """
     Establish connection to DB before execution request
     """
-    global dbase
-    dbase = g.get('link_db')
+    global db
+    db = g.get('link_db')
+    dbase = Flsql(db)
 
 
 @fllogin.teardown_request
@@ -53,8 +55,8 @@ def register():
             len(request.form['username']) > 4 and len(request.form['email']) > 4 and \
             len(request.form['psw']) > 4 and request.form['psw'] == request.form['psw2']
             ):
-            hash = generate_password_hash(request.form['psw'])
-            res = dbase.addUser(request.form['username'], request.form['email'], hash)
+            cur_hash = generate_password_hash(request.form['psw'])
+            res = dbase.addUser(request.form['username'], request.form['email'], cur_hash)
             if res:
                 flash('Вы успешно зарегистрированы','success')
                 return redirect(url_for('fllogin.index'))
